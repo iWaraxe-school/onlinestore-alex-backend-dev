e266a20c06df04d26f5e91ee27d4acb42f5927ba
Alexander Ulyanitsky <aulyanitisky@lt.life>
2022-04-06 22:25:49 +0300

    XMLParser realization.

--- consoleApp/src/main/java/StoreApp.java
+++ consoleApp/src/main/java/StoreApp.java
@@ -1,16 +1,28 @@
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Locale;
 import java.util.Scanner;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class StoreApp {
 
-    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ParserConfigurationException, SAXException {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ParserConfigurationException, SAXException, IOException {
 
         RandomStorePopulator randomStorePopulator = new RandomStorePopulator(new StoreHelper());
         var storeCreation = randomStorePopulator.createStore();
         System.out.println(storeCreation);
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+
+        XMLParser handler = new XMLParser();
+        parser.parse(new File("consoleApp/src/main/resources/config.xml"), handler);
+
+        var resultHandler = handler.getSort();
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
---
+++ consoleApp/src/main/java/XMLParser.java
@@ -0,0 +1,49 @@
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import java.util.LinkedHashMap;
+
+public class XMLParser extends DefaultHandler {
+
+    LinkedHashMap<String, String> sort = new LinkedHashMap<>();
+    private StringBuilder data = null;
+
+    public LinkedHashMap<String, String> getSort() {
+        return sort;
+    }
+
+    boolean bPrice = false;
+    boolean bName = false;
+    boolean bRate = false;
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if (qName.equalsIgnoreCase("name")) {
+            bName = true;
+        } else if (qName.equalsIgnoreCase("price")) {
+            bPrice = true;
+        } else if (qName.equalsIgnoreCase("rate")) {
+            bRate = true;
+        }
+        data = new StringBuilder();
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (bName) {
+            sort.put("name", data.toString());
+            bName = false;
+        } else if (bPrice) {
+            sort.put("price", data.toString());
+            bPrice = false;
+        } else if (bRate) {
+            sort.put("rate", data.toString());
+            bRate = false;
+        }
+    }
+
+    @Override
+    public void characters(char ch[], int start, int length) throws SAXException {
+        data.append(new String(ch, start, length));
+    }
+}
